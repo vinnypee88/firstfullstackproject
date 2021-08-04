de8a34ba2614b80b3b106212286a6d2861444bd8
Vinod Patel <v_p88@hotmail.com>
2021-08-04 16:13:48 +0100

    Added CRUD functionality to the item table and updated swagger

--- models/products-models.js
+++ models/products-models.js
@@ -21,6 +21,44 @@ class products {
       console.error(error);
     }
   };
+  postItem = async (req, res) => {
+    try {
+      const { id, itemName, modelNumber, description, stock, price, image } =
+        req.body;
+      const addItem = await pool.query(
+        "INSERT INTO item (id, item_name, model_number, description, stock, price, image) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+        [id, itemName, modelNumber, description, stock, price, image]
+      );
+      res.json(addItem.rows);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  updateItem = async (req, res) => {
+    try {
+      const { id, itemName, modelNumber, description, stock, price, image } =
+        req.body;
+      const updateItem = await pool.query(
+        "UPDATE item SET item_name = $1, model_number = $2, description = $3, stock = $4, price = $5, image = $6 WHERE id = $7 RETURNING *",
+        [itemName, modelNumber, description, stock, price, image, id]
+      );
+      res.json(updateItem.rows);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  deleteItem = async (req, res) => {
+    const { id } = req.body;
+    try {
+      const deleteItem = await pool.query(
+        "DELETE FROM item WHERE id = $1 RETURNING *",
+        [id]
+      );
+      res.json(deleteItem.rows);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 }
 
 module.exports = products;
--- routes/products.js
+++ routes/products.js
@@ -6,4 +6,10 @@ router.get("/", productsInstance.getItems);
 
 router.get("/:id", productsInstance.getItem);
 
+router.post("/", productsInstance.postItem);
+
+router.put("/", productsInstance.updateItem);
+
+router.delete("/", productsInstance.deleteItem);
+
 module.exports = router;
